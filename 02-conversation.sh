c76d6162590ba6903bb162c862250127ab6d99a6
praveena <praveenapalla4@gmail.com>
2025-12-09 12:51:36 +0530

    conversion file

--- 02-conversation.sh
+++ 02-conversation.sh
@@ -4,4 +4,4 @@ PERSON1=Jhon;
 PERSON2=Raheem;
 
 echo "$PERSON1 : Hi $PERSON2, How are you?"
-echo "$PERSON2: Hi $PERSON1, I am fine How are you?
+echo "$PERSON2: Hi $PERSON1, I am fine How are you?"
