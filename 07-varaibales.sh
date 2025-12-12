1b72f783b5b931c0ab8b4076abcdb3afdca671ee
praveena <praveenapalla4@gmail.com>
2025-12-12 12:07:24 +0530

    variables

--- 07-varaibales.sh
+++ 07-varaibales.sh
@@ -3,9 +3,9 @@
 DATE=$(date)
  echo "timestamp: $DATE"
 
- START_TIME=$(date+%S)
+ START_TIME=$(date +%S)
 
- END_TIME=$(date+%S)
+ END_TIME=$(date +%S)
 
  TOTAL_TIME=$(($END_TIME-$START_TIME))
  echo "Totaltime: $TOTAL_TIME"
