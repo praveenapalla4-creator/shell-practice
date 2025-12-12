45982d97ad998ebbcaba675601045e6e7b7ed629
praveena <praveenapalla4@gmail.com>
2025-12-12 14:58:13 +0530

    env-variabels

---
+++ 11-conditions.sh
@@ -0,0 +1,9 @@
+#!/bin/bash
+
+echo "Please enter the number"
+ NUMBER=$1
+ if [ $NUMBER % 2 -eq 0 ]; then
+    echo "given number $NUMBER is even"
+ else 
+    echo "Given number $NUMBER is odd"
+fi   
