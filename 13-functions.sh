ef70ca38721b0ae257268e3b2bc5deda2e99ce3c
praveena <praveenapalla4@gmail.com>
2025-12-12 15:41:10 +0530

    env-variabels

--- 13-functions.sh
+++ 13-functions.sh
@@ -7,29 +7,41 @@ if [ $USERID -ne 0 ]; then
    echo "ERROR: Please run this script with root privilege"
    exit 1 # if we not use this then it will proceed with next lines
 fi
-dnf install mysql -y
 
-if [ $? -ne 0 ]; then
-   echo " ERROR: Installing mysql is fialure"
+VALIDATE(){
+    if [ $1 -ne 0 ]; then
+   echo " ERROR: Installing $2 is fialure"
    exit 1
 else
-    echo "mysql is installed"
+    echo "$2 is installed"
 fi
+
+}
+dnf install mysql -y
+VALIDATE $? "MYSQL"
+
+# if [ $? -ne 0 ]; then
+#    echo " ERROR: Installing mysql is fialure"
+#    exit 1
+# else
+#     echo "mysql is installed"
+# fi
 
 dnf install redis -y
-
-if [ $? -ne 0 ]; then
-   echo " ERROR: Installing redis is fialure"
-   exit 1
-else
-    echo "redis is installed"
-fi
+VALIDATE $? "REDIS"
+# if [ $? -ne 0 ]; then
+#    echo " ERROR: Installing redis is fialure"
+#    exit 1
+# else
+#     echo "redis is installed"
+# fi
 
 dnf install python -y
+VALIDATE $? "PYTHON"
 
-if [ $? -ne 0 ]; then
-   echo " ERROR: Installing python3 is fialure"
-   exit 1
-else
-    echo "python3 is installed"
-fi
+# if [ $? -ne 0 ]; then
+#    echo " ERROR: Installing python3 is fialure"
+#    exit 1
+# else
+#     echo "python3 is installed"
+# fi
