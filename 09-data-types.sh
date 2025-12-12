1c2fc69536626eb7af3bccd3d18c79f6e137f6ea
praveena <praveenapalla4@gmail.com>
2025-12-12 14:32:28 +0530

    env-variabels

--- 09-data-types.sh
+++ 09-data-types.sh
@@ -6,3 +6,8 @@ NAME=DevOps
 
 SUM=$(($NUMBER1+$NUMBER2+$NAME))
 echo "Sum is : ${SUM}"
+
+LEADERS={"Modi","Babu","Jagan"}
+echo "All Leader: ${LEADERS[@]}"
+echo "First Learder: ${LEADERS[0]}"
+echo "Second Leader: ${LEADERS[1]}"
