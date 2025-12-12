f4f282aeac3d50f4b5d25ed1a06ceabd3411ce98
praveena <praveenapalla4@gmail.com>
2025-12-12 14:44:22 +0530

    env-variabels

---
+++ 10-conditions.sh
@@ -0,0 +1,13 @@
+#!/bin/bash
+NUMBER=$1
+if [ $NUMBER -lt 10 ];  then
+   echo "Given Number  $NUMBER is less than 10"
+elif [ $NUMBER -eq 10 ]; then
+echo "Given number $NUMBER is equal to 10"
+else
+   echo "Given Number $NUMBER is greater than 10"  
+fi
+
+#-gt- greater tha
+#-eq - equal to
+# -ne  not equal to
