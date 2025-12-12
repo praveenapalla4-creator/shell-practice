916b60acdcdfa01a86fb18d2122faa2e2168d728
praveena <praveenapalla4@gmail.com>
2025-12-12 11:37:58 +0530

    variables

--- 02-conversation.sh
+++ 02-conversation.sh
@@ -3,5 +3,5 @@
 PERSON1=Jhon;
 PERSON2=Raheem;
 
-echo "$PERSON1 : Hi $PERSON2, How are you?"
-echo "$PERSON2: Hi $PERSON1, I am fine How are you?"
+echo "Jhon : Hi Raheem, How are you?"
+echo "Raheem: Hi Jhon, I am fine How are you?"
