418612f7c8720074b0183fe86a02738287122d87
praveena <praveenapalla4@gmail.com>
2025-12-14 08:16:08 +0530

    loops

--- 18-loops.sh
+++ 18-loops.sh
@@ -6,7 +6,7 @@ R="\e[31m"
 G="\e[32m"
 Y="\e[33m"
 N="\e[0m"
-LOGS_FOLDER ="/var/log/shell-script"
+LOGS_FOLDER="/var/log/shell-script"
 SCRIPT_NAME=$( echo $0 | cut -d "." -f1)
 LOG_FILE="$LOGS_FOLDER/$SCRIPT_NAME.log"
 
