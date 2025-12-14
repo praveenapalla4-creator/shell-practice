b206b7ef392b9590e4cb96ccdc86d71d91c3f376
praveena <praveenapalla4@gmail.com>
2025-12-14 08:10:33 +0530

    loops

--- 16-logs.sh
+++ 16-logs.sh
@@ -6,9 +6,9 @@ R="\e[31m"
 G="\e[32m"
 Y="\e[33m"
 N="\e[0m"
-LOGS_FOLDER ="/var/log/shell-script"
+LOGS_FOLDER="/var/log/shell-script"
 SCRIPT_NAME=$( echo $0 | cut -d "." -f1)
-LOG_FILE="$LOGS_FOLDER/$SCRIPT_NAME.log"
+LOG_FILE="$LOGS_FOLDER/$SCRIPT_NAME.log" #/var/logs/shell-script/16-logs.log
 
 mkdir -p $LOGS_FOLDER
 echo "script started executed at: $(date)" | tee -a $LOG_FILE
