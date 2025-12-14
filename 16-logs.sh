ec48d2c7e93b171e08b869bf88f693f222557a39
praveena <praveenapalla4@gmail.com>
2025-12-14 07:55:54 +0530

    loops

---
+++ 16-logs.sh
@@ -0,0 +1,50 @@
+#!/bin/bash
+
+
+USERID=$(id -u)
+R="\e[31m"
+G="\e[32m"
+Y="\e[33m"
+N="\e[0m"
+LOGS_FOLDER ="/var/log/shell-script"
+
+if [ $USERID -ne 0 ]; then
+   echo -e "ERROR: Please run this script with root privilege"
+   exit 1 # if we not use this then it will proceed with next lines
+fi
+
+VALIDATE(){
+    if [ $1 -ne 0 ]; then
+   echo  -e " ERROR: Installing $2  ... $R is fialure $N"
+   exit 1
+else
+    echo -e "$2 ... $R is installed $N"
+fi
+
+}
+
+dnf list installed mysql
+if [ $? -ne 0 ]; then 
+    dnf install mysql -y
+    VALIDATE $? "MYSQL"
+else 
+    echo -e "MYSQL is already installed ..$Y SKIPPING $N"
+fi 
+
+dnf list installed redis
+if [ $? -ne 0 ]; then 
+   dnf install redis -y
+VALIDATE $? "REDIS"
+else 
+    echo -e "redis is already installed ..$Y SKIPPING $N"
+fi 
+
+dnf list installed python3
+if [ $? -ne 0 ]; then 
+    dnf install python3 -y
+    VALIDATE $? "PYTHON"
+
+else 
+    echo -e "python is already installed ..$Y SKIPPING $N"
+fi 
+
